2ea768d1484434ad95c511e23bc9092c0b21df8c
pauhull <pauhull3@gmail.com>
2023-03-05 14:02:04 +0100

    Remove bStats

--- src/main/java/eu/decentsoftware/holograms/api/DecentHolograms.java
+++ src/main/java/eu/decentsoftware/holograms/api/DecentHolograms.java
@@ -20,8 +20,6 @@ import eu.decentsoftware.holograms.api.world.WorldListener;
 import eu.decentsoftware.holograms.event.DecentHologramsReloadEvent;
 import lombok.Getter;
 import lombok.NonNull;
-import org.bstats.bukkit.Metrics;
-import org.bstats.charts.SingleLineChart;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -87,10 +85,6 @@ public final class DecentHolograms {
         PluginManager pm = Bukkit.getPluginManager();
         pm.registerEvents(new PlayerListener(), this.plugin);
         pm.registerEvents(new WorldListener(), this.plugin);
-
-        // Setup metrics
-        Metrics metrics = new Metrics(this.plugin, 12797);
-        metrics.addCustomChart(new SingleLineChart("holograms", () -> Hologram.getCachedHolograms().size()));
 
         // Setup update checker
         if (Settings.CHECK_FOR_UPDATES) {
